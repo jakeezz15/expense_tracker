15fd9c138d6170d551826a9041aac4e0016d1764
Jake Durante <jakeezz15@gmail.com>
2024-01-19 22:22:55 +0200

    Creating model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: 
-    ),
+  runApp(
+    const MaterialApp(home: Expenses()),
   );
 }
